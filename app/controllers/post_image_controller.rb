7c30a0b0e65832269ba6852fa708f2e45eed359d
EC2 Default User <ec2-user@ip-172-31-42-98.ap-northeast-1.compute.internal>
2024-11-07 14:44:16 +0000

    [finish] 4-13

--- app/controllers/post_image_controller.rb
+++ app/controllers/post_image_controller.rb
@@ -11,7 +11,7 @@ class PostImageController < ApplicationController
     @post_image = PostImage.new(post_image_params)
     @post_image.user_id = current_user.id
     @post_image.save
-    redirect_to post_images_path
+    redirect_to post_image_path
   end
 
   def index
@@ -20,6 +20,18 @@ class PostImageController < ApplicationController
 
   def show
     @post_image = PostImage.find(params[:id])
+  end
+
+  # def destroy
+    # post_image = PostImage.find(parama[:id])
+    # post_image.destroy
+    # redirect_to post_images_path
+  # end
+
+  def destroy
+    post_image = PostImage.find(params[:id])  
+    post_image.destroy  
+    redirect_to post_images_path  
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ Rails.application.routes.draw do
   # ↓部分　resources :post_images, only: [:new, :create, :index, :show]　だと永遠にルーティングエラー出た
   # 理由もよく分からないし、AIに全部投げたら以下のコード吐き出した。末尾にコントローラ：ポスト〜とか書いたら全部変わっちゃうんじゃないの？
   # というかそもそも末尾に繋げていいいの？　全然分からないんだけど。
-  resources :post_images, only: [:new, :create, :index, :show], controller: 'post_image'
+  resources :post_images, only: [:new, :create, :index, :show, :destroy], controller: 'post_image'
   get 'homes/about' => 'homes#about', as: 'about'
+
+
 end
